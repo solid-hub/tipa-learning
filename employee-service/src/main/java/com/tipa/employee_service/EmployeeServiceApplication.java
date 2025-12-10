89c76279cf7d28eb558cc73f372faa4d0403f9d4
solid_home <kpark5555@gmail.com>
2025-12-10 22:18:15 +0900

    feat: add employee-service spring boot 3.5 + jpa skeleton

---
+++ employee-service/src/main/java/com/tipa/employee_service/EmployeeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.tipa.employee_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main application class for the Employee Service microservice
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+
+}
